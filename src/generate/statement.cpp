698f13e80a524439c8d35da5c094fa03df478242
chrgro <chrgro@gmail.com>
2011-10-18 21:06:31 -0700

    Added function pointer in constructor

--- src/generate/statement.cpp
+++ src/generate/statement.cpp
@@ -1,9 +1,43 @@
+#ifndef STATEMENT_CPP
+#define STATEMENT_CPP
+
+#include <string>
+#include <cstdlib>
+#include "function.cpp"
+
 class Statement {
+private:	
+	Function *parent;
+    int type;
 	
 public:
 	
-	void print(std::ostream& out){
+
+    Statement(Function *parent) 
+    {
+        this->parent = parent;
+        int type = rand() % 2;
+        
+        switch (type) {
+            case 0: 
+                // Assign something to one of the available variables
+                
+                
+                break;
+            case 1:
+                // Create a new variable
+                
+                break;
+        }
+    
+    }
+	
+	void print(std::ostream& out)
+	{
+	    
 		out << "x = 1;" << std::endl;
 	}
 	
 } ;
+
+#endif
