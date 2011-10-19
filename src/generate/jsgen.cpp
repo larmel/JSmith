929c46bef7ffeca59ed18360bbdc1551a3b3fc7b
Stian <stian.pedersen@gmail.com>
2011-10-18 18:30:17 -0700

    Some channges.

--- src/generate/jsgen.cpp
+++ src/generate/jsgen.cpp
@@ -3,24 +3,30 @@
 
 class Program {
 public:
+	
     void print(std::ostream& out) {
-        out << "var x = 1" << std::endl;
-        out << "(function foo() { return x++; })();" << std::endl;
+        out << "var x = 100" << std::endl;
+        out << "function foo() { return x++; };" << std::endl;
     };
+	
+	void main(std::ostream& out) {
+		out << "(function main(){ hash = foo(); print(hash); return 0; })();";
+	}
 };
-
-
 
 int main(int argc, char* argv[]) {
     
-    std::cout << "JSmith" << std::endl;
+    std::cout << "/*\tJSmith\t*/" << std::endl;
     
     // Read parameters on how random/how large etc
     
     // Intantiate new random program, and print it to stdout (or to file)
     
-    (new Program())->print(std::cout);
-    
+	Program c;
+	
+	c.print(std::cout);
+	c.main(std::cout);
+	
     return 0;
 }
 
