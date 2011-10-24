ef78ab34844b7210055247191bbb55a3378f1a6c
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2011-10-23 18:40:02 -0700

    Deleted commented code

--- src/generate/Variable.cpp
+++ src/generate/Variable.cpp
@@ -2,7 +2,7 @@
 #include "Variable.h"
 
 Variable::Variable(Type t) {
-    this->name = "default_var"; // Variable::getNewRandomIdentifier();
+    this->name = "default_var";
     this->type = t;
 }
 
@@ -10,32 +10,3 @@ Variable::Variable(std::string s, Type t) {
     this->name = s;
     this->type = t;
 }
-/*
-Variable* Variable::getRandomLocal(Function* f, Type t) {
-    // TODO: Recur on parent functions
-    
-    if (f->context.size() == 0) return NULL;
-    int pos = rand() % f->context.size();
-    return f->context[pos];
-}
-
-Variable* Variable::getRandomGlobal() {
-    if (Program::context.size() == 0) return NULL;
-    int pos = rand() % Program::context.size();
-    return Program::context[pos];
-}
-
-std::string Variable::getNewRandomIdentifier(Function* f) {
-    // TODO: Need to check that it is unique
-    char navn[5];
-	while (true) {
-		for (int i = 0; i < 4; i++) {
-			int j = rand() % number_of_valid_identifier_characters;
-			navn[i] = valid_identifier_characters[j];
-		}
-		navn[4] = '\0';
-		break;
-	}
-	
-	return navn;
-}*/
