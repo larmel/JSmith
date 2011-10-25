b76ee67d84476aaa19914a54f3adb3e62248fb67
chrgro <chrgro@gmail.com>
2011-10-24 20:26:03 -0700

    Added add/mul/div/sub expression type

---
+++ src/generate/AddMulExpression.cpp
@@ -0,0 +1,31 @@
+#include "AddMulExpression.h"
+#include "Variable.h"
+
+AddMulExpression::AddMulExpression(Scope* parent_scope, int depth) : Expression(parent_scope, depth) {
+    
+    // Get variables to work on:
+    // TODO: Other types than NUMBER_T aswell?
+
+    this->op1 = Expression::generate_expression(this->scope, this->depth+1);
+    this->op2 = Expression::generate_expression(this->scope, this->depth+1);
+    if (this->op1==NULL) 
+        std::cerr << "No operator 1\n";   
+    if (this->op1==NULL) 
+        std::cerr << "No operator 2\n";
+    
+    // Pick the operator 
+    int op_id = rand() % 4;
+    switch (op_id) {
+    case 0: this->op = "+"; break;
+    case 1: this->op = "-"; break;
+    case 2: this->op = "*"; break;
+    case 3: this->op = "/"; break;
+    }
+}
+
+void AddMulExpression::print(std::ostream& out, unsigned int depth) {
+    // Print variable
+    op1->print(out, depth);
+    out << this->op;
+    op2->print(out, depth);
+}
---
+++ src/generate/AddMulExpression.h
@@ -0,0 +1,33 @@
+#ifndef ADDMULEXPRESSION_H
+#define ADDMULEXPRESSION_H 
+ 
+#include <iostream>
+#include <cstdlib>
+
+#include "Expression.h"
+
+class Variable;
+class Scope;
+
+// Class for addition, subtraction, multiplication and division
+// in expressions.
+ 
+class AddMulExpression : public Expression {
+
+private:
+    // Operands:
+    Expression *op1;
+    Expression *op2;
+    
+    // Operator:
+    std::string op;
+    
+public:
+
+    AddMulExpression(Scope* parent_scope, int depth);
+    
+    void print(std::ostream& out, unsigned int depth);
+};
+ 
+ 
+ #endif
