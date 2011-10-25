dca026a98763e157da33b2943ccea9ca03dff703
chrgro <chrgro@gmail.com>
2011-10-24 21:26:47 -0700

    Relational expressions

--- src/generate/Expression.cpp
+++ src/generate/Expression.cpp
@@ -2,6 +2,7 @@
 
 #include "PrimaryExpression.h"
 #include "AddMulExpression.h"
+#include "RelationalExpression.h"
 //TODO: Create subclasses to instantiate instead
 
 Expression::Expression(Scope *scope, int depth) 
@@ -13,6 +14,7 @@ Expression::Expression(Scope *scope, int depth)
     
     this->scope = scope;
     this->depth = depth;
+    this->parenthesis = false;
 }
 
 Expression *Expression::generate_expression(Scope *scope, int depth) {
@@ -21,8 +23,11 @@ Expression *Expression::generate_expression(Scope *scope, int depth) {
     int gen_type = rand() % 100;
     
     // TODO: So far this only creates 1 new expression, not utilizing the list
+    // Using several should work though.
+    
     Expression *expression = new Expression(scope, depth);
     
+
     // Linear increasing probability for terminal expression nodes
     int p_terminal = 50 + 50*((double)depth/10);
 
@@ -30,10 +35,6 @@ Expression *Expression::generate_expression(Scope *scope, int depth) {
     {
         // PrimaryExpression (actual variables)
         Expression *subexpr = new PrimaryExpression(scope, depth);
-        if (subexpr==NULL) {
-            std::cerr << "No expressionvariable returned";
-            exit(1);
-        }
         expression->expressions.push_back(subexpr);
         
     } 
@@ -43,20 +44,25 @@ Expression *Expression::generate_expression(Scope *scope, int depth) {
     {
         gen_type = rand() % 100;
         
-        if (gen_type<100) // Generate a AddMulExpression
+        if (gen_type<70) // Generate a AddMulExpression
         {
             Expression *subexpr = new AddMulExpression(scope, depth);
-            if (subexpr==NULL) {
-                std::cerr << "No AddMulExpression returned";
-                exit(1);
-            }
             expression->expressions.push_back(subexpr);
-        } 
+        }
         
-        else if (gen_type < 100) // Generate RelationalExpression 
+        else if (gen_type < 90) // Generate <,>,>=,<= Expression
         {
-            // TODO: This stuff
+            Expression *subexpr = new RelationalExpression(scope, depth);
+            expression->expressions.push_back(subexpr);
         
+        }
+        
+        else if (gen_type < 100) // Generate '(' Expression ')'
+        {
+            Expression *subexpr = Expression::generate_expression(scope, depth+1);
+            expression->expressions.push_back(subexpr);
+            
+            expression->parenthesis = true;
         }
         
     }
@@ -69,11 +75,16 @@ Expression *Expression::generate_expression(Scope *scope, int depth) {
 // Print function
 void Expression::print(std::ostream &out, unsigned int depth)
 {
+    if (this->parenthesis) out << "(";
+    
     for (int i = 0; i<this->expressions.size(); i++) {
         this->expressions[i]->print(out, 1);
         if (i<this->expressions.size()-1) {
             out << ",";        
         }
     }
+    
+    if (this->parenthesis) out << ")";
+    
 }
 
