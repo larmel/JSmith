b86aa9a03d191b9f68362563f8144d8e880b3ede
Stian <stian.pedersen@gmail.com>
2011-10-24 21:13:34 -0700

    BlockStatements

--- test/generated.js
+++ test/generated.js
@@ -1,25 +1,46 @@
 /*	JSmith	*/
-function OOJRg() {
-   if (1.78017e+09) {
+function hQkdc() {
+   function pkhLo() {
+      if (2.06957e+08)
+         var KlfnB = 1.68324e+09*2.07461e+08-1.3472e+09-1.00187e+09;
+      function papgf() {
+         var oPSqG = 6.74303e+08;
+         if (oPSqG-4.25345e+08*2.04885e+09-1.9885e+09/oPSqG*oPSqG-oPSqG*5.31559e+08+5.41322e+08)
+         {
+
+         }
+         if (oPSqG+oPSqG-1.97759e+09/1.29035e+08+oPSqG)
+         {
+
+         }
+         var hbCOG = 1.88416e+09*oPSqG*2.34111e+08;
+      }
+      var QnHqF = 1.56816e+09;
+      if (9.46111e+07)
+      {
+
+      }
    }
-   var CtBGE = 8.41492e+08/1.61842e+09;
-   if (1.04502e+09) {
+   if (1.81326e+09)
+   {
+
    }
-   function dbGmf() {
-      if (1.63639e+09) {
-      }
-      if (1.03326e+09) {
-      }
-      var lskpE = 1.89932e+08/1.9701e+09;
-      if (lskpE) {
-      }
+   var FHSqF = 3.1342e+08/1.14111e+09+7.71974e+08*5.7103e+08*1.41556e+09;
+   if (1.81037e+09-1.73317e+09)
+   {
+
    }
 }
-if (2.00329e+08) {
+if (4.71445e+08)
+   if (2.30306e+08)
+   {
+
+   }
+if (1.35162e+09/1.23421e+09/1.88067e+09)
+{
+
 }
-var MtTJt = 1.71425e+09;
-if (MtTJt+MtTJt) {
-}
+var rQifL = 1.95478e+09/1.63746e+09;
 (function () {
-   print(MtTJt); 
+   print(rQifL); 
 })();
