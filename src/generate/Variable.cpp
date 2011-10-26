6f9b3a8cbf74c13a25532507ca9eaccdd658e184
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2011-10-26 02:36:20 -0700

    Moved static variables inside class scope

--- src/generate/Variable.cpp
+++ src/generate/Variable.cpp
@@ -2,6 +2,26 @@
 #include "Random.h"
 #include <cstdlib>
 
+int Variable::reserved_word_count = 29;
+std::string Variable::reserved_word[] = {
+    "break̈́",  "do", "instanceof", "typeof",
+    "case", "else", "new", "var",
+    "catch", "finally", "return", "void",
+    "continue", "for", "switch", "while",
+    "debugger", "function", "this", "with",
+    "default", "if", "throw",
+    "delete", "in", "try",
+    "null",
+    "true", "false"
+};
+
+int Variable::identifier_character_count = 42;
+char Variable::identifier_character[] = {
+    '$', '_',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'
+};
+
 Variable::Variable(std::string s, Type t) {
     this->name = s;
     this->type = t;
@@ -15,13 +35,13 @@ std::string Variable::generateRandomIdentifier()
     do {
         identifier = "";
         do {
-            char nextchar = identifier_character[ rand() % identifier_characters ];
+            char nextchar = identifier_character[ rand() % identifier_character_count ];
             identifier += nextchar;
         } while (Random::flip_coin());
         
         is_reserved = false;
-        for (int i = 0; i < reserved_words; ++i) {
-            if (identifier == reserverd_word[i]) {
+        for (int i = 0; i < reserved_word_count; ++i) {
+            if (identifier == reserved_word[i]) {
                 is_reserved = true;
                 break;
             }
--- src/generate/Variable.h
+++ src/generate/Variable.h
@@ -4,26 +4,6 @@
 #include <string>
 #include <vector>
 
-static const int reserved_words = 29;
-static const std::string reserverd_word[] = {
-    "break̈́",  "do", "instanceof", "typeof",
-    "case", "else", "new", "var",
-    "catch", "finally", "return", "void",
-    "continue", "for", "switch", "while",
-    "debugger", "function", "this", "with",
-    "default", "if", "throw",
-    "delete", "in", "try",
-    "null",
-    "true", "false"
-};
-
-static const int identifier_characters = 42;
-static const char identifier_character[] = {
-    '$', '_',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'
-};
-
 enum Type {
     UNDEFINED_T, NULL_T, BOOLEAN_T, NUMBER_T, STRING_T, OBJECT_T, FUNCTION_T
 };
@@ -38,6 +18,12 @@ public:
     Variable(std::string s, Type t);
     
     static std::string generateRandomIdentifier();
+    
+    static int reserved_word_count;
+    static std::string reserved_word[];
+    
+    static int identifier_character_count;
+    static char identifier_character[];
 };
 
 #endif
