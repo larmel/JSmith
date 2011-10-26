d0f50a0ff9373ddcc5d1ad87dbf6cb7568ca76d6
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2011-10-25 17:35:31 -0700

    Added call to random initialization

--- src/generate/jsgen.cpp
+++ src/generate/jsgen.cpp
@@ -3,20 +3,35 @@
 #include <time.h>
 
 #include "Program.h"
-#include "random.h"
+#include "Random.h"
 
+/*
+ * Usage:
+ * > ./generate <seed>
+ */
 int main(int argc, char* argv[]) {
     
-    Random::seed(0x876544L);
+    unsigned long s = time(NULL);
     
-    std::cout << "/*\tJSmith\t*/" << std::endl;
+    // Can give a seed as parameter to reconstruct some program
+    // TODO: Error checking etc
+    if (argc > 1) {
+        unsigned long s = atol(argv[1]);
+    }
     
-    srand ( time(NULL) );
+    // This decides the whole generated source
+    Random::seed( s );
     
+    // Create an AST from subset of grammar
 	Program c;
 	
+    std::cout << "/**********************************************/" << std::endl;
+    std::cout << "/*  Automatically generated script by JSmith    " << std::endl;
+    std::cout << "/*  Seed: " << s                                  << std::endl;
+    std::cout << "/**********************************************/" << std::endl;
+    
+	// Print generated program
 	c.print(std::cout);
 	
     return 0;
-
 }
