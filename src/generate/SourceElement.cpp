2a76b82fb1000604c6d2d9a5ebc16e7110a2380a
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2011-11-06 00:09:10 -0700

    Added overload << operator for SourceElement

--- src/generate/SourceElement.cpp
+++ src/generate/SourceElement.cpp
@@ -18,3 +18,17 @@ SourceElement* SourceElement::createRandom(Scope* parent_scope, unsigned int dep
 	return Statement::newRandomStatement(parent_scope, depth);
 }
 
+/*
+ * Simply print the correct number of spaces for indentation.
+ * Derived classes should call this before other print code. 
+ */
+std::ostream& operator<<(std::ostream& out, const SourceElement& e) {
+    for (int i = 0; i < e.depth; ++i) {
+        for (int j = 0; j < e.indentation; ++j) {
+            out << " ";
+        }
+    }
+    return out;
+}
+
+
--- src/generate/SourceElement.h
+++ src/generate/SourceElement.h
@@ -11,6 +11,10 @@ class Scope;
  */
 class SourceElement
 {
+private:
+    // Number of space characters to print
+    static const int indentation = 4;
+
 protected:
     // Will either be Program or a FunctionDeclaration
     Scope* scope;
@@ -23,8 +27,10 @@ public:
     // This class knows about it's subclasses, perhaps not so elegant?
     static SourceElement* createRandom(Scope* parent_scope, unsigned int depth);
     
-    // Pure virtual, abstract class
+    // Pure virtual, abstract class TODO: Remove, use overloading instead
     virtual void print(std::ostream &out) = 0;
+    
+    friend std::ostream& operator<<(std::ostream& out, const SourceElement& e);
 };
 
 #endif
