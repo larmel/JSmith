5bc79c4f067901658d2d693ecc4d0604e983ee78
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2011-11-15 21:42:53 -0800

    Minimized program that gives (small) numerical error

---
+++ test/bugs/001_minimized.js
@@ -0,0 +1,14 @@
+/*
+ * ### Test Summary November 15, 2011 at 21:29:52
+ * ### Time Taken: Average 316 ms.  Best KJS 12 ms. Worst Rhino 778 ms).
+ *
+ * SpiderMonkey: (1, 5.458913070090713e+27)
+ * Rhino: (1, 5.458913070090713e+27)
+ * V8: (1, 5.458913070090714e+27)
+ * KJS: (1, 5.458913070090714e+27)
+ * Narcissus: (1, 5.458913070090713e+27)
+ */
+var ObGId = (1435238133*(1889579895*2012875688)+2069886681);
+(function () {
+   print(ObGId); 
+})();
