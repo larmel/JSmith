efd211c7a72616676fc5a248ca78538ecfe27ffe
Stian Pedersen <stian.pedersen@gmail.com>
2011-11-16 00:15:28 -0800

    CallExpression gives arguments

--- src/generate/Expression/CallExpression.cpp
+++ src/generate/Expression/CallExpression.cpp
@@ -1,23 +1,23 @@
 #include "CallExpression.h"
 #include "Variable.h"
 #include "Scope.h"
-
+#include <iostream>
 
 CallExpression::CallExpression(Scope* parent_scope, int depth) : Expression(parent_scope, depth) {
 
     // Fetch a local function (we're only allowed to call functions on the same level)
-    this->function = parent_scope->getRandomFunctionVariable();
-    for(int i = 0; i < this->function->getNumArguments(); i++){
-    	this->parameters.push_back(Expression::generateExpression(parent_scope, depth));
-    }
 
-    // TODO Create expressions to use as args
+    this->function = parent_scope->getRandomFunctionVariable();
+    if(this->function != NULL){
+		for(int i = 0; i < this->function->getNumArguments(); i++){
+			this->parameters.push_back(Expression::generateExpression(parent_scope, depth));
+		}
+    }
 }
 
 void CallExpression::print(std::ostream& out) const {
     if (this->function == NULL)
     {
-        // No functions available, what to do?
         out << "1";
     }
     else
@@ -25,14 +25,13 @@ void CallExpression::print(std::ostream& out) const {
         out << *this->function << "(";
         for(int i = 0; i < this->function->getNumArguments(); i++){
         	out << *this->parameters.at(i);
-        	if(i !=this->function->getNumArguments()){
+        	if(i != this->function->getNumArguments() - 1){
 				out << ", ";
         	}
         }
         out << ")";
     }
 }
-
 
 std::ostream& operator<<(std::ostream& out, const CallExpression& e) {
     e.print(out);
