4e52583cf414abe866099335c3e166b2f902c37e
Stian Pedersen <stian.pedersen@gmail.com>
2011-11-29 17:24:51 -0800

    renamed the generate

---
+++ src/generate/generate.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+#include "Program.h"
+#include "Random.h"
+#include <sys/time.h>
+
+/*
+ * Usage:
+ * > ./generate <seed>
+ */
+int main(int argc, char* argv[]) {
+         
+    // Get the time seed
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    unsigned long s = time(NULL);
+    unsigned long time_seed = s*1000000+tv.tv_usec;
+    
+    // Can give a seed as parameter to reconstruct some program
+    // TODO: Error checking etc
+    if (argc > 1) {
+        time_seed = atol(argv[1]);
+    }
+    
+    // This decides the whole generated source
+    Random::seed( time_seed );
+    
+    // Create an AST from subset of grammar
+	Program c;
+	
+    std::cout << "/********************************************************/" << std::endl;
+    std::cout << "/*  JSmith (seed " << s << ")" << std::endl;
+    std::cout << "/********************************************************/" << std::endl;
+    
+	// Print generated program
+	c.print(std::cout);
+	
+    return 0;
+}
